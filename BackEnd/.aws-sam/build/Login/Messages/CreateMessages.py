d2134a5ebd328d513b8def3388f4f75dca2801c8
snutt10 <snutt@student.neumont.edu>
2026-08-25 15:46:40 -0600

    Some signup issues fixed

--- BackEnd/.aws-sam/build/Login/Messages/CreateMessages.py
+++ BackEnd/.aws-sam/build/Login/Messages/CreateMessages.py
@@ -18,8 +18,11 @@ from decimal import Decimal
 from os import getenv
 from uuid import uuid4
 from botocore.awsrequest import AWSRequest
-from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
-from gremlin_python.process.anonymous_traversal import traversal
+#from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
+#from gremlin_python.process.anonymous_traversal import traversal
+
+table = boto3.resource('dynamodb').Table(getenv('TABLE_NAME', 'Channels'))
+
 
 def response(code, body):
     return {
@@ -39,24 +42,50 @@ def _decimal_to_float(obj):
 
 def lambda_handler(event, context):
     try:
-        # Extract message data from the event body
+        #get data from body
         body = json.loads(event.get("body", "{}"))
         channel_id = body.get("channel_id")
-        channel_name = body.get("channel_name")
         user_id = body.get("user_id")
-        user_name = body.get("user_name")
         message_content = body.get("message")
 
-        if not channel_id or not channel_name or not user_id or not user_name or not message_content:
+        #required fields
+        if not channel_id or not user_id or not message_content:
             return response(400, {"error": "Missing required fields"})
 
+        #make sure the channel exists
+        existing = table.get_item(Key={"Id": channel_id})
+        if "Item" not in existing:
+            return response(404, {"message": f"Channel '{channel_id}' not found"})
+        
         # Generate a unique message ID
         message_id = str(uuid4())
 
         # Get the current timestamp in UTC
         timestamp = datetime.now(timezone.utc).isoformat()
 
-        return response(200, {"message": "Message created successfully", "message_id": message_id})
+        #create message for insertion
+        newMessage = {
+            "messageID" : message_id,
+            "userID" : user_id,
+            "message" : message_content,
+            "timestamp" : timestamp
+        }
+        #formatting thing for append, it wants it as a list
+        newMessageList = [newMessage]
+
+        #update the channel, appending the message
+        response = table.update_item(
+            Key={
+                'ID': channel_id  # Your table's primary key
+            },
+            UpdateExpression="SET messages = list_append(messages, :new_vals)",
+            ExpressionAttributeValues={
+                ":new_vals": newMessageList  # The list of values to append
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+
+        return response(200, {"message": "Message created successfully", "message_id": message_id, "response Attributes":response['Attributes']})
 
     except Exception as e:
         logging.error(f"Error creating message: {str(e)}")
--- BackEnd/.aws-sam/build/SignUpUsers/Messages/GetAllMessages.py
+++
@@ -1,2 +0,0 @@
-# GETS only the last 100 messages from the database, ordered by timestamp in descending order.
-import json
